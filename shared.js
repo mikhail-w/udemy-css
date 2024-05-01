58527b77f4b2d61bfc522bb7d58d0cb04017d1e6
Mikhail Waddell <mikhail.waddell@gmail.com>
2024-04-30 20:29:22 -0400

    add mobile nav to all pages

--- shared.js
+++ shared.js
@@ -1,20 +1,65 @@
 const backdrop = document.querySelector('.backdrop');
-const buttons = document.querySelectorAll('.plan button');
+const selectPlanButtons = document.querySelectorAll('.plan button');
 const modal = document.querySelector('.modal');
-const hideModal = document.querySelector('.modal__action--negative');
+const closeModalBtn = document.querySelector('.modal__action--negative');
+const toggleBtn = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
 
-function toggleModal(state) {
-  console.log('Inside', state);
-  backdrop.style.display = state ? 'block' : 'none';
-  modal.style.display = state ? 'block' : 'none';
-}
-
-buttons.forEach(btn => {
+selectPlanButtons.forEach(btn => {
   btn.addEventListener('click', () => {
     toggleModal(true);
   });
 });
 
-hideModal.addEventListener('click', () => {
-  toggleModal(false);
+backdrop.addEventListener('click', () => {
+  hideBackdrop();
+  hideMobileNav();
+  if (modal) {
+    hideModal();
+  }
 });
+
+toggleBtn.addEventListener('click', () => {
+  showMobileNav();
+  showBackdrop();
+});
+
+if (closeModalBtn) {
+  closeModalBtn.addEventListener('click', () => {
+    toggleModal(false);
+  });
+}
+
+function toggleModal(state) {
+  if (state) {
+    showModal();
+    showBackdrop();
+  } else {
+    hideBackdrop();
+    hideModal();
+  }
+}
+
+function showModal() {
+  modal.classList.add('open');
+}
+
+function showBackdrop() {
+  backdrop.classList.add('open');
+}
+
+function showMobileNav() {
+  mobileNav.classList.add('open');
+}
+
+function hideModal() {
+  modal.classList.remove('open');
+}
+
+function hideBackdrop() {
+  backdrop.classList.remove('open');
+}
+
+function hideMobileNav() {
+  mobileNav.classList.remove('open');
+}
